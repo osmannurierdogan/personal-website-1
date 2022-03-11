938cde48db951f3b48f2ebab3f3fa417aaf6cf52
Osman Nuri Erdoğan <osmerd04@gmail.com>
2022-03-11 16:08:41 +0300

    commit after relocation of files

--- index.php
+++ index.php
@@ -5,24 +5,26 @@
  * @version: 1.0.1
  */
 
-get_header();
+//get_header();
 
-get_template_part( 'template-parts/page-title/page-title', 'blog' );
+//get_template_part( 'template-parts/page-title/page-title', 'blog' );
 
 ?>
+<?php include 'header.php' ?>
 
 <main class="vlt-main vlt-main--padding">
-
+	
 	<?php
 
-		// Elementor `archive` location
-		if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'archive' ) ) {
-			get_template_part( 'template-parts/archive/archive', 'blog' );
-		}
+// Elementor `archive` location
+if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'archive' ) ) {
+	get_template_part( 'template-parts/archive/archive', 'blog' );
+}
 
-	?>
+?>
 
 </main>
 <!-- /.vlt-main -->
 
-<?php get_footer(); ?>
+<?php //get_footer(); ?>
+<?php include 'footer.php' ?>
